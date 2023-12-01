94cbfa0a2319a8356e867832206ea3933cce87df
zeeshantaj <zeeshantaj.zt@gmail.com>
2023-12-01 19:14:36 +0500

    first2 commit

--- app/src/main/java/com/example/food_map_application/MainActivity.java
+++ app/src/main/java/com/example/food_map_application/MainActivity.java
@@ -7,20 +7,33 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieComposition;
+import com.airbnb.lottie.LottieCompositionFactory;
+import com.airbnb.lottie.LottieListener;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -28,7 +41,15 @@ import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -69,29 +90,51 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
-        // Define LatLngBounds for Karachi
-        LatLng southwest = new LatLng(24.7408, 66.9881); // Southwest bounds of Karachi
-        LatLng northeast = new LatLng(25.2736, 67.4204); // Northeast bounds of Karachi
-
-        karachiBounds = new LatLngBounds(southwest, northeast);
-
-        map.setLatLngBoundsForCameraTarget(karachiBounds);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(karachiBounds.getCenter(), 12.0f));
-
-        map.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
-            @Override
-            public void onCameraMove() {
-                if (!karachiBounds.contains(map.getCameraPosition().target)) {
-                    // If the camera moves outside Karachi bounds, move it back to Karachi
-                    map.moveCamera(CameraUpdateFactory.newLatLngBounds(karachiBounds, 0));
-                }
-            }
-        });
-
+//        // Define LatLngBounds for Karachi
+//        LatLng sydney = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()); // Southwest bounds of Karachi
+//
 //        map.addMarker(new MarkerOptions()
 //                .position(sydney)
 //                .title("My Location"));
 //        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Define LatLng for the current location
+        LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
+        // Custom marker view with Lottie animation
+        View markerView = LayoutInflater.from(this).inflate(R.layout.custom_marker_layout, null);
+
+        // Find the LottieAnimationView within the custom marker layout
+        LottieAnimationView animationView = markerView.findViewById(R.id.animation_view);
+
+
+        // Set animation for the LottieAnimationView
+        animationView.setAnimation(R.raw.location_anim_);
+        animationView.loop(true);
+        animationView.playAnimation();
+
+        // Add marker with custom view to the map
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(currentLatLng)
+                .title("My Location")
+                .icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(this, markerView)));
+
+        map.addMarker(markerOptions);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12.0f));
+
+
+    }
+
+    private Bitmap createDrawableFromView(Context context, View view) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+        return bitmap;
     }
 
     @Override
@@ -117,6 +160,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         int id = item.getItemId();
+        if (id == R.id.none){
+            map.setMapType(GoogleMap.MAP_TYPE_NONE);
+        }
         if (id == R.id.normal){
             map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         }
